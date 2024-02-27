3028a394892e7b1ca2326a72e501b18cc6792295
İsmail Can Varlı <icanvarli@outlook.com>
2024-02-27 22:25:53 +0300

    Completed compose application.

--- ComposePractice/app/src/main/java/com/example/composepractice/MainActivity.kt
+++ ComposePractice/app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ShowImage(
+                        firstText = stringResource(R.string.jetpack_compose_tutorial),
+                        secondText = stringResource(R.string.jetpack_compose_modern),
+                        thirdText = stringResource(R.string.in_this_tutorial),
+                        modifier = Modifier
+                            .fillMaxSize()
+                    )
                 }
             }
         }
@@ -30,17 +47,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ShowImage(
+    firstText: String, secondText: String, thirdText: String, modifier: Modifier = Modifier
+) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(
         modifier = modifier
-    )
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = firstText,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(24.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = secondText,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = thirdText,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ShowImagePreview() {
     ComposePracticeTheme {
-        Greeting("Android")
+        ShowImage(
+            firstText = stringResource(R.string.jetpack_compose_tutorial),
+            secondText = stringResource(R.string.jetpack_compose_modern),
+            thirdText = stringResource(R.string.in_this_tutorial)
+        )
     }
 }
