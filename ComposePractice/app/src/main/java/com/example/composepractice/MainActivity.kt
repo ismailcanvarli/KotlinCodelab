ed9b9e7799ad5e8c8d73b3b531056ccd3b9beca5
İsmail Can Varlı <icanvarli@outlook.com>
2024-02-27 22:26:48 +0300

    Import section modified

--- ComposePractice/app/src/main/java/com/example/composepractice/MainActivity.kt
+++ ComposePractice/app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
