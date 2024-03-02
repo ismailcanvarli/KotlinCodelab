15ef2420ac4df2973083630a8a3d969a6451bef0
İsmail Can Varlı <icanvarli@outlook.com>
2024-03-02 12:22:33 +0300

    Code line breaks clean up.

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,23 +8,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.diceroller.ui.theme.DiceRollerTheme
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,8 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiceRollerTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     DiceRollerApp()
                 }
@@ -52,7 +51,6 @@ fun DiceRollerApp() {
     )
 }
 
-
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     //result değerini şöyle tanımladık.
@@ -69,8 +67,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     }
 
     Column( //Buton ile görseli alt alta sıralamak için kullandık.
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally //Ortaladık
+        modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally //Ortaladık
     ) {
         Image( //Zar resmi koyacağımız için bir image oluşturduk
             painter = painterResource(imageResource),
