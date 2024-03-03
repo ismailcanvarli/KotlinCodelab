779d98ae672572cd91a1b2382b2b37ece3713085
İsmail Can Varlı <icanvarli@outlook.com>
2024-03-03 23:33:37 +0300

    final commit: Lemonade app

--- Lemonade/app/src/main/java/com/example/lemonade/ui/theme/Theme.kt
+++ Lemonade/app/src/main/java/com/example/lemonade/ui/theme/Theme.kt
@@ -38,10 +38,9 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun LemonadeTheme(
+fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
