5bd5e4c98d4e43b54cbc8a27ff17b8f5bab7cf00
İsmail Can Varlı <icanvarli@outlook.com>
2024-03-04 21:04:35 +0300

    final commit: State hoisting (component stateless) was used.

--- basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -50,6 +50,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
+/*
+başlangıç değeri olşturduk
+aşağıda bir başlangıç değerli değişken duruma sahibi değişken tanımladık
+Bu değeri burada tanımlamamızın sebebi buna başka composable'dan da erişmek
+için kullandık.
+ */
+    var amountInput by remember {mutableStateOf("")}
+
+    //klavyeden girilen değeri aldık ve double'a çevirdik
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -66,12 +78,14 @@ fun TipTimeLayout() {
         )
         //Text'in altına ekledik
         EditNumberField(
+            value = amountInput,
+            onValueChange = {amountInput = it},
             modifier = Modifier
                 .padding(bottom = 32.dp) //alt kısmına boşluk ekledik
                 .fillMaxWidth() //yatayda tüm genişlik sağlayacak
         )
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -89,23 +103,24 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
+/*Bu composable'da bulunan text'te yazan bilgi değişkenine başka composable'dan
+* erişmek için bu değeri bu composable'a bir değişken olarak vereceğiz
+* hangi composable'a lazımsa bu değer ona değişken olarak vereceğiz
+ */
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier) {
-    //başlangıç değeri olşturduk
-    //aşağıda bir başlangıç değerli değişken duruma sahibi değişken tanımladık
-    var amountInput by remember {mutableStateOf("")}
-
-    //klavyeden girilen değeri aldık ve double'a çevirdik
-    val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+    ) {
 
     //aşağıda ise textfield'daki değişken değerden gelen value'yi ona eşitledik
     TextField(
-        value = amountInput, //bu textfield'daki bulunan değer için
+        value = value, //bu textfield'daki bulunan değer için
         //tutar değerini textfield'daki değişen değere eşitliyoruz.
         //it ifadesinin kullanılma nedeni kendisine yani yeni değere eşitledik demek
         //buda textfield'da değer değiştiğinde çalışıyor
-        onValueChange = {amountInput = it },
+        onValueChange = onValueChange,
         //Text'in içine label ekledik ve giriş yapacağı şeyin iblgisini veriyoruz
         label = { Text(stringResource(R.string.bill_amount)) },
         //Tek bir satırda yazılmasını istiyoruz aşağı satıra geçmesin
