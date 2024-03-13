3f503558f2307dccb3e0c483cf90def512c94719
İsmail Can Varlı <icanvarli@outlook.com>
2024-03-13 15:56:48 +0300

    Shape added to card view.

--- basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/ui/theme/Shape.kt
+++ basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/ui/theme/Shape.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
+//Burada şekilleri ve büyüklüklerini ekliyoruz.
 val Shapes = Shapes(
     small = RoundedCornerShape(50.dp),
+    medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp)
 )
