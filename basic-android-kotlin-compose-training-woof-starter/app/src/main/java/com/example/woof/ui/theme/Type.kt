e59c7797c8dadd4c466ff3acd9782e7f55a08c07
İsmail Can Varlı <icanvarli@outlook.com>
2024-03-13 16:21:48 +0300

    Typography(text size, text style) added.

--- basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/ui/theme/Type.kt
+++ basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/ui/theme/Type.kt
@@ -1,22 +1,50 @@
 /*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+Burada da yazı tiplerini fontlarını ekliyoruz.
+Buraya eklediğimiz fontları öncesinde res klasörünün altına ekledik
+orada font isimli bir android directory açtık.
+sonrasıda google font denen web sitesinden istediğimiz fontları seçtik
+isimlerini düzelttik ve buraya ekledik
  */
 package com.example.woof.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.woof.R
+import androidx.compose.ui.text.TextStyle
 
-// Set of Material typography styles to start with
+//bunlar eklediğimiz yazı tipleri
+val AbrilFatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
+
+// Kullandığımız yazı tiplerini değiştirdik
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = AbrilFatface,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    )
 )
