038c53c46bd424410c7380cdeac384b825d6054c
İsmail Can Varlı <icanvarli@outlook.com>
2024-03-18 15:18:21 +0300

    Initial commit: Starter code for Dessert Clicker.

---
+++ dessert-clicker-starter/app/src/main/java/com/example/dessertclicker/model/Dessert.kt
@@ -0,0 +1,7 @@
+package com.example.dessertclicker.model
+
+/**
+ * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
+ * Model dosyaları data class'lardan oluşur ve data sınıflarının nasıl olacağını belirtir.
+ */
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
