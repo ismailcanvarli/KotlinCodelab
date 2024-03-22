0c2235d7de1b21643c9ba5f16cb670abf4519d9c
İsmail Can Varlı <icanvarli@outlook.com>
2024-03-22 14:42:31 +0300

    Write unit tests for ViewModel - Error path testing

--- basic-android-kotlin-compose-training-unscramble-starter/app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
+++ basic-android-kotlin-compose-training-unscramble-starter/app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
@@ -5,6 +5,7 @@ import com.example.unscramble.data.getUnscrambledWord
 import com.example.unscramble.ui.GameViewModel
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class GameViewModelTest {
@@ -14,14 +15,16 @@ class GameViewModelTest {
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
     }
 
-    //Succes path için bu fonksiyonu oluşturacağız.
     /*
-    Note: The code above uses the thingUnderTest_TriggerOfTest_ResultOfTest
-    format to name the test function name:
-    thingUnderTest = gameViewModel
-    TriggerOfTest = CorrectWordGuessed
-    ResultOfTest = ScoreUpdatedAndErrorFlagUnset
+    Test fonksiyonu nasıl isimlendirilmelidir.
+        Note: The code above uses the thingUnderTest_TriggerOfTest_ResultOfTest
+        format to name the test function name:
+        thingUnderTest = gameViewModel
+        TriggerOfTest = CorrectWordGuessed
+        ResultOfTest = ScoreUpdatedAndErrorFlagUnset
      */
+
+    //Succes path için bu fonksiyonu oluşturacağız.
     @Test
     fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
         var currentGameUiState = viewModel.uiState.value
@@ -41,5 +44,23 @@ class GameViewModelTest {
         assertEquals(20, currentGameUiState.score)
     }
 
+    /*
+    Error path (yanlış değerler) için bu testi oluşturacağız.
+    Yanlış kelime vereceğiz ve sonucun ne olacağına bakacağız.
+     */
+    @Test
+    fun gameViewModel_IncorrectGuess_ErrorFlagSet() {
+        //doğru olmayan kelimemiz
+        val incorrectPlayerWord = "and"
 
+        viewModel.updateUserGuess(incorrectPlayerWord)
+        viewModel.checkUserGuess()
+
+        val currentGameUiState = viewModel.uiState.value
+        // Oyuncunun puanının değişmediğini iddia ediyoruz
+        assertEquals(0, currentGameUiState.score)
+        // doğru kelimenin kullanıcının girdiği kelime olduğunu iddia ediyoruz
+        assertTrue(currentGameUiState.isGuessedWordWrong)
+
+    }
 }
