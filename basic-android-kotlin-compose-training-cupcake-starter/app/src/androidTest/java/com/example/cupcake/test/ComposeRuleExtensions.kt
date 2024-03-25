c9a0b79172ce6c9f209260114e90d784ead2d2d8
İsmail Can Varlı <icanvarli@outlook.com>
2024-03-25 21:53:56 +0300

    Write navigation tests - import section is modified.

--- basic-android-kotlin-compose-training-cupcake-starter/app/src/androidTest/java/com/example/cupcake/test/ComposeRuleExtensions.kt
+++ basic-android-kotlin-compose-training-cupcake-starter/app/src/androidTest/java/com/example/cupcake/test/ComposeRuleExtensions.kt
@@ -4,13 +4,8 @@ import androidx.activity.ComponentActivity
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
 
 /*
 aşağıdaki fonskiyonu yazma amacımız bir resource'la işlem yaparken
@@ -23,34 +18,3 @@ composeTestRule.onNodeWithStringId(R.string.my_string)
 fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.onNodeWithStringId(
     @StringRes id: Int
 ): SemanticsNodeInteraction = onNodeWithText(activity.getString(id))
-
-private fun navigateToFlavorScreen() {
-    composeTestRule.onNodeWithStringId(R.string.one_cupcake).performClick()
-    composeTestRule.onNodeWithStringId(R.string.chocolate).performClick()
-}
-
-private fun getFormattedDate(): String {
-    val calendar = Calendar.getInstance()
-    calendar.add(java.util.Calendar.DATE, 1)
-    val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
-    return formatter.format(calendar.time)
-}
-
-private fun navigateToPickupScreen() {
-    navigateToFlavorScreen()
-    composeTestRule.onNodeWithStringId(R.string.next)
-        .performClick()
-}
-
-private fun navigateToSummaryScreen() {
-    navigateToPickupScreen()
-    composeTestRule.onNodeWithText(getFormattedDate())
-        .performClick()
-    composeTestRule.onNodeWithStringId(R.string.next)
-        .performClick()
-}
-
-private fun performNavigateUp() {
-    val backText = composeTestRule.activity.getString(R.string.back_button)
-    composeTestRule.onNodeWithContentDescription(backText).performClick()
-}
