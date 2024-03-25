1d12a72196ba88867b255621e9a481673bf7946a
İsmail Can Varlı <icanvarli@outlook.com>
2024-03-25 21:40:32 +0300

    Write navigation tests - Write more helper methods.

--- basic-android-kotlin-compose-training-cupcake-starter/app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
+++ basic-android-kotlin-compose-training-cupcake-starter/app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -4,6 +4,7 @@ import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
@@ -12,6 +13,9 @@ import com.example.cupcake.CupcakeScreen
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 //Uygulama açıldığında direk test navigation controller oluşturuyoruz.
 private lateinit var navController: TestNavHostController
@@ -60,4 +64,35 @@ fun cupcakeNavHost_clickOneCupcake_navigatesToSelectFlavorScreen() {
     composeTestRule.onNodeWithStringId(R.string.one_cupcake)
         .performClick()
     navController.assertCurrentRouteName(CupcakeScreen.Flavor.name)
+}
+
+private fun navigateToFlavorScreen() {
+    composeTestRule.onNodeWithStringId(R.string.one_cupcake).performClick()
+    composeTestRule.onNodeWithStringId(R.string.chocolate).performClick()
+}
+
+private fun getFormattedDate(): String {
+    val calendar = Calendar.getInstance()
+    calendar.add(java.util.Calendar.DATE, 1)
+    val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+    return formatter.format(calendar.time)
+}
+
+private fun navigateToPickupScreen() {
+    navigateToFlavorScreen()
+    composeTestRule.onNodeWithStringId(R.string.next)
+        .performClick()
+}
+
+private fun navigateToSummaryScreen() {
+    navigateToPickupScreen()
+    composeTestRule.onNodeWithText(getFormattedDate())
+        .performClick()
+    composeTestRule.onNodeWithStringId(R.string.next)
+        .performClick()
+}
+
+private fun performNavigateUp() {
+    val backText = composeTestRule.activity.getString(R.string.back_button)
+    composeTestRule.onNodeWithContentDescription(backText).performClick()
 }
