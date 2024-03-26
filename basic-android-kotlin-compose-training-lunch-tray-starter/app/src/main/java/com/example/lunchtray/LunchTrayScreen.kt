08265de4a1bc239ebed025c3dbabd2cae2387739
İsmail Can Varlı <icanvarli@outlook.com>
2024-03-26 10:34:34 +0300

    enum class created for route and top bar.

--- basic-android-kotlin-compose-training-lunch-tray-starter/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ basic-android-kotlin-compose-training-lunch-tray-starter/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -1,20 +1,6 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.lunchtray
 
+import androidx.annotation.StringRes
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -24,6 +10,14 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.lunchtray.ui.OrderViewModel
 
 // TODO: Screen enum
+//Buradaki güncelleme işlemi yapıldı. top bar da gözükecek.
+enum class CupcakeScreen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    EntreeMenu(title = R.string.choose_entree),
+    SideDishMenu(title = R.string.choose_side_dish),
+    AccompanimentMenu(title = R.string.choose_accompaniment),
+    Checkout(title = R.string.order_checkout)
+}
 
 // TODO: AppBar
 
