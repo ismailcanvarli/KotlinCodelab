779917c99c34b2979679666525b71f799761a2d2
İsmail Can Varlı <icanvarli@outlook.com>
2024-04-04 23:50:03 +0300

    Add repository and Manual Dependency Injection - Write a ViewModel test.

--- mars-photos/app/src/test/java/com/example/marsphotos/NetworkMarsRepositoryTest.kt
+++ mars-photos/app/src/test/java/com/example/marsphotos/NetworkMarsRepositoryTest.kt
@@ -1,6 +1,8 @@
-package com.example.marsphotos.fake
+package com.example.marsphotos
 
 import com.example.marsphotos.data.NetworkMarsPhotosRepository
+import com.example.marsphotos.fake.FakeDataSource
+import com.example.marsphotos.fake.FakeMarsApiService
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
