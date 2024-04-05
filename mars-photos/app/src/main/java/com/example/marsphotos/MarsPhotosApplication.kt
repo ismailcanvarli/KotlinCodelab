6822295ddf00d5d8ad45ed7d5353e72cb505cc29
İsmail Can Varlı <icanvarli@outlook.com>
2024-04-05 16:52:46 +0300

    final commit: Mars photo app.

--- mars-photos/app/src/main/java/com/example/marsphotos/MarsPhotosApplication.kt
+++ mars-photos/app/src/main/java/com/example/marsphotos/MarsPhotosApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import com.example.marsphotos.data.AppContainer
 import com.example.marsphotos.data.DefaultAppContainer
 
-// Uygulama seviyesindeki DI(Dependency Injection) konteyneri.
+// Bu sınıf, uygulama başlatıldığında oluşturulur ve uygulama seviyesindeki DI konteynerini oluşturur.
 class MarsPhotosApplication: Application() {
     //
     lateinit var container: AppContainer
